a274e63429b7650fb4964ca1e7d041d625795320
Hannah Johnson <hlj.inbox@gmail.com>
2016-06-20 12:26:19 -0500

    Add facebook login controller

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -7,6 +7,107 @@ angular.module('app.controllers', [])
 ////////////////////////////////////////////////////////////////////////////////
 
 .controller('loginCtrl', function ($scope, dataBase, $rootScope, $state, $ionicPopup, $ionicHistory, $timeout) {
+  // This is the success callback from the login method
+  var fbLoginSuccess = function(response) {
+    if (!response.authResponse){
+      fbLoginError("Cannot find the authResponse");
+      return;
+    }
+
+    var authResponse = response.authResponse;
+
+    getFacebookProfileInfo(authResponse)
+    .then(function(profileInfo) {
+      // For the purpose of this example I will store user data on local storage
+      UserService.setUser({
+        authResponse: authResponse,
+				userID: profileInfo.id,
+				name: profileInfo.name,
+				email: profileInfo.email,
+        picture : "http://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
+      });
+      $ionicLoading.hide();
+      $state.go('app.home');
+    }, function(fail){
+      // Fail get profile info
+      console.log('profile info fail', fail);
+    });
+  };
+
+  // This is the fail callback from the login method
+  var fbLoginError = function(error){
+    console.log('fbLoginError', error);
+    $ionicLoading.hide();
+  };
+
+  // This method is to get the user profile info from the facebook api
+  var getFacebookProfileInfo = function (authResponse) {
+    var info = $q.defer();
+
+    facebookConnectPlugin.api('/me?fields=email,name&access_token=' + authResponse.accessToken, null,
+      function (response) {
+				console.log(response);
+        info.resolve(response);
+      },
+      function (response) {
+				console.log(response);
+        info.reject(response);
+      }
+    );
+    return info.promise;
+  };
+
+  //This method is executed when the user press the "Login with facebook" button
+  $scope.facebookSignIn = function() {
+    facebookConnectPlugin.getLoginStatus(function(success){
+      if(success.status === 'connected'){
+        // The user is logged in and has authenticated your app, and response.authResponse supplies
+        // the user's ID, a valid access token, a signed request, and the time the access token
+        // and signed request each expire
+        console.log('getLoginStatus', success.status);
+
+    		// Check if we have our user saved
+    		var user = UserService.getUser('facebook');
+
+    		if(!user.userID){
+					getFacebookProfileInfo(success.authResponse)
+					.then(function(profileInfo) {
+						// For the purpose of this example I will store user data on local storage
+						UserService.setUser({
+							authResponse: success.authResponse,
+							userID: profileInfo.id,
+							name: profileInfo.name,
+							email: profileInfo.email,
+							picture : "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
+						});
+
+						$state.go('app.home');
+					}, function(fail){
+						// Fail get profile info
+						console.log('profile info fail', fail);
+					});
+				}else{
+					$state.go('app.home');
+				}
+      } else {
+        // If (success.status === 'not_authorized') the user is logged in to Facebook,
+				// but has not authenticated your app
+        // Else the person is not logged into Facebook,
+				// so we're not sure if they are logged into this app or not.
+
+				console.log('getLoginStatus', success.status);
+
+				$ionicLoading.show({
+          template: 'Logging in...'
+        });
+
+				// Ask the permissions you need. You can learn more about
+				// FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+        facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
+      }
+    });
+  };
+
     $scope.login = function(user) {
         var usr = getUserByLogin(dataBase.Users, user.userName, user.password);
 
@@ -218,11 +319,11 @@ function validateUser($rootScope, $state) {
 
 /**
  * Adds a new user to the database
- * @param users    - the users JSON object in the database service 
+ * @param users    - the users JSON object in the database service
  * @param name     - the name of the user to add
  * @param email    - the email of the user to add
  * @param userName - the username of the user to add
- * @param password - the password of the user to add 
+ * @param password - the password of the user to add
  *                   TODO: Hash the password in this function or beforehand
  * @param imgPath  - the path to the profile image of the user to add
  * @param enabled  - the state of the users account, true if access is allowed
@@ -230,14 +331,14 @@ function validateUser($rootScope, $state) {
 function addUser(users, name, email, userName, password, imgPath, enabled) {
     var now = new Date();
     var tempId = 1;
-    
+
     users.some(function (user) {
         if (user.UserId >= tempId) {
             tempId = user.UserId + 1;
         }
     });
 
-    var usr = 
+    var usr =
     {
         UserId: tempId,
         Name: name,
@@ -352,7 +453,7 @@ function deleteConversations(dataBase, childId) {
 /**
  * Returns a user object based off of their UserId, if they exist
  * @param  users  - the users JSON object in the database service
- * @param  userId - the userId of the user to be retrieved 
+ * @param  userId - the userId of the user to be retrieved
  */
 function getUserById(users, userId) {
     var usr = null;
@@ -369,7 +470,7 @@ function getUserById(users, userId) {
 /**
  * Returns a user object based off of their UserName, if they exist
  * @param  users    - the users JSON object in the database service
- * @param  userName - the userName of the user to be retrieved 
+ * @param  userName - the userName of the user to be retrieved
  */
 function getUserByUserName(users, userName) {
     var usr = null;
@@ -429,14 +530,14 @@ function getUserByLogin(users, userName, password) {
 function addChild(children, userId, name, birthDay, imgPath) {
     var now = new Date();
     var tempId = 1;
-    
+
     children.some(function (child) {
         if (child.ChildId >= tempId) {
             tempId = child.ChildId + 1;
         }
     });
 
-    var chld = 
+    var chld =
     {
         ChildId: tempId,
         ParentId: userId,
